4446f5916c84af16f9c28ea4f9c3e92959e7203b
Younggyo Seo <seo0gyo@gmail.com>
2019-03-15 18:36:48 +0900

    changed readme and edited eval.py

--- eval.py
+++ eval.py
@@ -1,14 +1,11 @@
-import _pickle as pickle
 import os
 
-import zmq
 import torch
-from tensorboardX import SummaryWriter
 import numpy as np
 
 import utils
-from wrapper import make_atari, wrap_atari_dqn
 from model import DuelingDQN
+from wrapper import make_atari, wrap_atari_dqn
 from arguments import argparser
 
 
@@ -19,16 +16,7 @@ def get_environ():
 
 
 def main():
-    learner_ip = get_environ()
     args = argparser()
-
-    writer = SummaryWriter(comment="-{}-eval".format(args.env))
-
-    ctx = zmq.Context()
-    param_socket = ctx.socket(zmq.SUB)
-    param_socket.setsockopt(zmq.SUBSCRIBE, b'')
-    param_socket.setsockopt(zmq.CONFLATE, 1)
-    param_socket.connect('tcp://{}:52001'.format(learner_ip))
 
     env = make_atari(args.env)
     env = wrap_atari_dqn(env, args)
@@ -38,18 +26,14 @@ def main():
     env.seed(seed)
 
     model = DuelingDQN(env)
+    model.load_state_dict(torch.load('model.pth', map_location='cpu'))
 
-    data = param_socket.recv(copy=False)
-    param = pickle.loads(data)
-    model.load_state_dict(param)
-    print("Loaded first parameter from learner")
-
-    episode_reward, episode_length, episode_idx = 0, 0, 0
+    episode_reward, episode_length = 0, 0
     state = env.reset()
     while True:
         if args.render:
             env.render()
-        action, _ = model.act(torch.FloatTensor(np.array(state)), 0.01)
+        action, _ = model.act(torch.FloatTensor(np.array(state)), 0.)
         next_state, reward, done, _ = env.step(action)
 
         state = next_state
@@ -58,16 +42,9 @@ def main():
 
         if done:
             state = env.reset()
-            writer.add_scalar("eval/episode_reward", episode_reward, episode_idx)
-            writer.add_scalar("eval/episode_length", episode_length, episode_idx)
+            print("Episode Length / Reward: {} / {}".format(episode_length, episode_reward * 20))
             episode_reward = 0
             episode_length = 0
-            episode_idx += 1
-
-            if episode_idx % args.eval_update_interval == 0:
-                data = param_socket.recv(copy=False)
-                param = pickle.loads(data)
-                model.load_state_dict(param)
 
 
 if __name__ == '__main__':

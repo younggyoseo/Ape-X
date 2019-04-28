Change Log 190428 1. Edited eval.py to update parameter every episode 2. Edited wrapper.py to fix LazyFrames bug(See Issue #1) 3. Changed calculation method of loss from .mean() to .sum()

eval.py
@@ -1,7 +1,9 @@
+"""
+Module for evaluator in Ape-X.
+"""
 import _pickle as pickle
 import os
 from multiprocessing import Process, Queue
-import queue
 
 import zmq
 import torch
@@ -62,16 +64,15 @@ def exploration(args, actor_id, param_queue):
     param = None
     print("Received First Parameter!")
 
-    episode_reward, episode_length, episode_idx, actor_idx = 0, 0, 0, 0
+    episode_reward, episode_length, episode_idx = 0, 0, 0
     state = env.reset()
     while True:
-        action, q_values = model.act(torch.FloatTensor(np.array(state)), 0.)
+        action, _ = model.act(torch.FloatTensor(np.array(state)), 0.)
         next_state, reward, done, _ = env.step(action)
 
         state = next_state
         episode_reward += reward
         episode_length += 1
-        actor_idx += 1
 
         if done or episode_length == args.max_episode_length:
             state = env.reset()
@@ -80,14 +81,9 @@ def exploration(args, actor_id, param_queue):
             episode_reward = 0
             episode_length = 0
             episode_idx += 1
-
-        if actor_idx % args.update_interval == 0:
-            try:
-                param = param_queue.get(block=False)
-                model.load_state_dict(param)
-                print("Updated Parameter..")
-            except queue.Empty:
-                pass
+            param = param_queue.get()
+            model.load_state_dict(param)
+            print("Updated Parameter..")
 
 
 def main():

utils.py
@@ -57,7 +57,7 @@ def compute_loss(model, tgt_model, batch, n_steps, gamma=0.99):
     prios = (td_error + 1e-6).data.cpu().numpy()
 
     loss = torch.where(td_error < 1, 0.5 * td_error ** 2, td_error - 0.5)
-    loss = (loss * weights).mean()
+    loss = (loss * weights).sum()
     return loss, prios
 
 
@@ -67,11 +67,11 @@ def update_parameters(loss, model, optimizer, max_norm):
     """
     optimizer.zero_grad()
     loss.backward()
-    torch.nn.utils.clip_grad.clip_grad_norm_(model.parameters(), max_norm)
     total_norm = 0.
     for p in model.parameters():
         param_norm = p.grad.data.norm(2)
         total_norm += param_norm ** (1. / 2)
     total_norm = total_norm ** (1. / 2)
+    torch.nn.utils.clip_grad.clip_grad_norm_(model.parameters(), max_norm)
     optimizer.step()
     return total_norm

wrapper.py
@@ -188,6 +188,22 @@ class FrameStack(gym.Wrapper):
         return LazyFrames(list(self.frames))
 
 
+class TorchFrameStack(FrameStack):
+    def __init__(self, env, k):
+        """
+        Stack k first frames instead of last frames.
+        See FrameStack
+        """
+        super(TorchFrameStack, self).__init__(env, k)
+        shp = env.observation_space.shape
+        self.observation_space = spaces.Box(low=0, high=255, shape=((shp[0] * k,) + shp[1:]),
+                                            dtype=env.observation_space.dtype)
+
+    def _get_ob(self):
+        assert len(self.frames) == self.k
+        return TorchLazyFrames(list(self.frames))
+
+
 class ScaledFloatFrame(gym.ObservationWrapper):
     def __init__(self, env):
         gym.ObservationWrapper.__init__(self, env)
@@ -226,6 +242,14 @@ class LazyFrames(object):
 
     def __getitem__(self, i):
         return self._force()[i]
+
+
+class TorchLazyFrames(LazyFrames):
+    def _force(self):
+        if self._out is None:
+            self._out = np.concatenate(self._frames, axis=0)
+            self._frames = None
+        return self._out
 
 
 def make_atari(env_id, max_episode_steps=None):
@@ -281,7 +305,8 @@ class ImageToPyTorch(gym.ObservationWrapper):
     def __init__(self, env):
         super(ImageToPyTorch, self).__init__(env)
         old_shape = self.observation_space.shape
-        self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=(old_shape[-1], old_shape[0], old_shape[1]),
+        self.observation_space = gym.spaces.Box(low=0, high=255,
+                                                shape=(old_shape[-1], old_shape[0], old_shape[1]),
                                                 dtype=np.uint8)
 
     def observation(self, observation):
@@ -289,10 +314,16 @@ class ImageToPyTorch(gym.ObservationWrapper):
 
 
 def wrap_atari_dqn(env, args):
-    env = wrap_deepmind(env,
-                        episode_life=args.episode_life,
-                        clip_rewards=args.clip_rewards,
-                        frame_stack=args.frame_stack,
-                        scale=args.scale)
+    if args.episode_life:
+        env = EpisodicLifeEnv(env)
+    if 'FIRE' in env.unwrapped.get_action_meanings():
+        env = FireResetEnv(env)
+    env = WarpFrame(env)
+    if args.scale:
+        env = ScaledFloatFrame(env)
+    if args.clip_rewards:
+        env = ClipRewardEnv(env)
     env = ImageToPyTorch(env)
+    if args.frame_stack:
+        env = TorchFrameStack(env, 4)
     return env